d8d2373c4be521025dd0089eacb226fbe949a5da
Ananasaaa <kairyakn@gmail.com>
2024-05-26 23:43:39 +0300

    lesson_3.2

---
+++ script.js
@@ -0,0 +1,7 @@
+// Homework 3.2
+
+const userName = prompt("Enter your name");
+const userAge = Number(prompt("Enter your age"));
+const userCountry = prompt("Enter your country");
+
+console.log(`Hello, my name is ${userName}, i'm ${userAge} years old, and i'm from ${userCountry}`);
